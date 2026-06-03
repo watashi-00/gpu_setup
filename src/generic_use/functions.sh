e16d983008a0f9bc60b66683f5f7f32c478f6a3f
watashi-00 <kalitajj374@gmail.com>
2026-06-03 15:48:26 -0300

    feat(functions): add get_target_home function to retrieve user home directory under sudo

--- src/generic_use/functions.sh
+++ src/generic_use/functions.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+# Get the home directory of the actual user, even if running under sudo.
+get_target_home() {
+    if [ -n "${SUDO_USER:-}" ] && [ "$SUDO_USER" != "root" ]; then
+        getent passwd "$SUDO_USER" | cut -d: -f6
+    else
+        printf '%s\n' "${HOME:-/root}"
+    fi
+}
+
